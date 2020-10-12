05a7bf40ff5ce6f2ce096304b48b74a2a0712d03
sokolb <sokolb@mac.com>
2020-10-12 18:23:06 -0400

    restyled coming soon page for heroku for EVH

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -65,101 +65,109 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          Gear Caddy
-          <nav>
-            {this.state.user ? (
-              <>
-                <NavLink exact to="/">
-                  Pedals LIST
-                </NavLink>
-                &nbsp;&nbsp;&nbsp;
-                <NavLink exact to="/add">
-                  ADD Pedal
-                </NavLink>
-                &nbsp; &nbsp;&nbsp;
-                <Link to="" onClick={this.handleLogout}>
-                  LOG OUT
-                </Link>
-                &nbsp;&nbsp;&nbsp;
-              </>
-            ) : (
-              <>
-                <NavLink exact to="/login">
-                  LOG IN
-                </NavLink>
-                &nbsp;&nbsp;&nbsp;
-                <NavLink exact to="/signup">
-                  SIGN UP
-                </NavLink>
-              </>
-            )}
-          </nav>
-        </header>
-        <main>
-          {this.state.user ? (
-            <h2>Welcome, {this.state.user.username}</h2>
-          ) : (
-            <h2>You are not logged in!</h2>
-          )}
-          <Switch>
-            <Route
-              exact
-              path="/"
-              render={() => (
-                <PedalListPage
-                  user={this.state.user}
-                  pedals={this.state.pedals}
-                  handleDeletePedal={this.handleDeletePedal}
-                />
-              )}
-            />
-            <Route
-              exact
-              path="/add"
-              render={() => (
-                <AddPedalPage handleAddPedal={this.handleAddPedal} />
-              )}
-            />
-            <Route
-              exact
-              path="/details"
-              render={({ location }) => <PedalDetailPage location={location} />}
-            />
-            <Route
-              exact
-              path="/edit"
-              render={({ location }) => (
-                <EditPedalPage
-                  handleUpdatePedal={this.handleUpdatePedal}
-                  location={location}
-                />
-              )}
-            />
-            <Route
-              exact
-              path="/signup"
-              render={({ history }) => (
-                <SignupPage
-                  history={history}
-                  handleSignupOrLogin={this.handleSignupOrLogin}
-                />
-              )}
-            />
-            <Route
-              exact
-              path="/login"
-              render={({ history }) => (
-                <LoginPage
-                  history={history}
-                  handleSignupOrLogin={this.handleSignupOrLogin}
-                />
-              )}
-            />
-          </Switch>
-        </main>
+      // <div className="App">
+      //   <header className="App-header">
+      //     Gear Caddy
+      //     <nav>
+      //       {this.state.user ? (
+      //         <>
+      //           <NavLink exact to="/">
+      //             Pedals LIST
+      //           </NavLink>
+      //           &nbsp;&nbsp;&nbsp;
+      //           <NavLink exact to="/add">
+      //             ADD Pedal
+      //           </NavLink>
+      //           &nbsp; &nbsp;&nbsp;
+      //           <Link to="" onClick={this.handleLogout}>
+      //             LOG OUT
+      //           </Link>
+      //           &nbsp;&nbsp;&nbsp;
+      //         </>
+      //       ) : (
+      //         <>
+      //           <NavLink exact to="/login">
+      //             LOG IN
+      //           </NavLink>
+      //           &nbsp;&nbsp;&nbsp;
+      //           <NavLink exact to="/signup">
+      //             SIGN UP
+      //           </NavLink>
+      //         </>
+      //       )}
+      //     </nav>
+      //   </header>
+      //   <main>
+      //     {this.state.user ? (
+      //       <h2>Welcome, {this.state.user.username}</h2>
+      //     ) : (
+      //       <h2>You are not logged in!</h2>
+      //     )}
+      //     <Switch>
+      //       <Route
+      //         exact
+      //         path="/"
+      //         render={() => (
+      //           <PedalListPage
+      //             user={this.state.user}
+      //             pedals={this.state.pedals}
+      //             handleDeletePedal={this.handleDeletePedal}
+      //           />
+      //         )}
+      //       />
+      //       <Route
+      //         exact
+      //         path="/add"
+      //         render={() => (
+      //           <AddPedalPage handleAddPedal={this.handleAddPedal} />
+      //         )}
+      //       />
+      //       <Route
+      //         exact
+      //         path="/details"
+      //         render={({ location }) => <PedalDetailPage location={location} />}
+      //       />
+      //       <Route
+      //         exact
+      //         path="/edit"
+      //         render={({ location }) => (
+      //           <EditPedalPage
+      //             handleUpdatePedal={this.handleUpdatePedal}
+      //             location={location}
+      //           />
+      //         )}
+      //       />
+      //       <Route
+      //         exact
+      //         path="/signup"
+      //         render={({ history }) => (
+      //           <SignupPage
+      //             history={history}
+      //             handleSignupOrLogin={this.handleSignupOrLogin}
+      //           />
+      //         )}
+      //       />
+      //       <Route
+      //         exact
+      //         path="/login"
+      //         render={({ history }) => (
+      //           <LoginPage
+      //             history={history}
+      //             handleSignupOrLogin={this.handleSignupOrLogin}
+      //           />
+      //         )}
+      //       />
+      //     </Switch>
+      //   </main>
+      // </div>
+      <>
+      <div className="ComingSoon">
+        <h1>Gear Caddy Coming Soon!</h1>
       </div>
+      <div className="RIP">
+        <h3>Rest in Peace Eddie!</h3>
+      </div>
+    </>
     );
   }
 }
